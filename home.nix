8757f7ec4a53ceae72a11a9bb6eb9ca4ff296f8e
Aiden Tepper <aidenjtep@gmail.com>
2025-12-02 20:31:58 -0500

    packages without configs

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
 {
   home = {
     packages = with pkgs; [
-      hello
+      cbonsai
     ];
 
     username = "localaiden";
@@ -10,8 +10,33 @@
 
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
-    stateVersion = "23.11";
+    stateVersion = "25.11";
   };
 
   programs.home-manager.enable = true;
+
+  programs.lazygit.enable = true;
+  programs.bat.enable = true;
+  programs.fd.enable = true;
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.git = {
+    enable = true;
+    settings.user.name = "Aiden Tepper";
+    settings.user.email = "aidenjtep@gmail.com";
+  };
 }
