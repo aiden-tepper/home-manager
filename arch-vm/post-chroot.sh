77e5fe0db9cd51cb1fe9fe7fd81d2454bf555a20
Aiden Tepper <aiden-tepper@users.noreply.github.com>
2026-01-06 21:16:08 -0500

    Create post-chroot.sh for Btrfs snapshot
    Add script to create a read-only snapshot of the OS.

---
+++ arch-vm/post-chroot.sh
@@ -0,0 +1,13 @@
+# We need to access the top-level subvolume one last time
+mount -t btrfs -o subvolid=5 /dev/vda2 /mnt
+
+# NOW we take the snapshot. 
+# Since @ is currently mounted and active as /mnt/@, this captures the installed OS.
+btrfs subvolume snapshot -r /mnt/@ /mnt/@blank
+
+# Verify it exists
+ls /mnt
+# Should see: @  @blank  @home  @nix ...
+
+umount /mnt
+reboot
